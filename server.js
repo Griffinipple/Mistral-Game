0b99eedc84426fa6c33af87977f09ce46243b672
Skibidibble <168710375+Griffinipple@users.noreply.github.com>
2025-01-27 19:40:26 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname)));
 
 io.on('connection', (socket) => {
     console.log('New client connected');
